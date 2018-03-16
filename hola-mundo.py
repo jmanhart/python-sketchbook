9f6bfd58105ad1e759fdf23e9f891900a1aa46b9
Manhart, John <manhartjohn@gmail.com>
2018-03-15 21:37:58 -0500

    pypypython

--- hola-mundo.py
+++ hola-mundo.py
@@ -1,5 +1,13 @@
-name = input("What is your name: ")
-age = int(input("How old are you: "))
-year = str((2014 - age)+100)
+# name = input("What is your name: ")
+# age = int(input("How old are you: "))
+# year = str((2014 - age)+100)
+#
+# print(name + " will be 100 years old in the year " + year)
 
-print(name + " will be 100 years old in the year " + year)
+
+num = input("Enter a number: ")
+mod = num % 2
+if mod > 0:
+    print("You picked an odd number.")
+else:
+    print("You picked an even number.")
