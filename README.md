396d1f7762e609873e8eb0a42e5bdf978e870242
Manhart, John <manhartjohn@gmail.com>
2018-07-22 12:27:27 -0500

    Pushing up latest and greatest

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
+
+<!-- ![Framer Sketch Book](https://i.imgur.com/bQjnKTs.png) -->
+
 # python-sketchbook
+
+🐍 Collection of basics and my progress of learning Python.
+
+---------
+
+### framer-basics/
+A collection of useful code snippets and resources I have found helpful with Framer. This is forever a work in progress and has each project in here has detail comments
+Topics:
+- Loops
+- Classes
+- Conditonals **[WIP]**
+- Functions **[WIP]**
+- Using Modules **[Coming Soon...]**
+- JS Objects **[Coming Soon...]**
+
+
+
+
+### framer-designs/
+Sample projects and tests using the Design tab portion of Framer.
+
+
+### framer-sketchs/
+One off projects testing and exprimenting with aspects of the Framer API. 99% of projects in this directoy and rough and meant as "Sketchs".
--- basics-classes.py
+++ basics-classes.py
@@ -1,39 +1,51 @@
+import random
 
-
-class guessEntry:
-
-    guess_type = "Rando"
-    guess_color = "pink"
-
-    def __init__(self, guessAttempt):
-        self.guessAttempt = guessAttempt
-
-    def set_guessAmount(self, guessAmount):
-        input("This is the {} " .format(guessAmount))
-
-
-guessEntry(3)
-
-# def main():
-#     first = guessEntry(1)
+numMin = 1
+numMax = 100
 #
-#     # print(first.guessAttempt)
-#     #
-#     # first.set_guessAmount(667)
-#     input(first.set_guessAmount)
+randomNumber = random.randrange(numMin, numMax)
+
+guess = 40
+correct = 41
+
+
+class question:
+    def __init__(self, attempt, guess):
+        self.attempt = int(attempt)
+        self.guess = int(guess)
+
+        if guess == correct:
+            print("CORRECT! :) ")
+        elif guess != correct:
+            print("WRONG! Try again?")
 
 
 
-# if __name__ == "__main__":
-#     main()
+
+
+
+def main():
+    first = question(1, 40)
+    # print(correct)
 
 
 
 
 
+if __name__ == "__main__":
+    main()
 
 
 
+
+
+#
+# first.set_guessAmount(667)
+# input(first.set_guessAmount)
+# def set_guessAmount(self, guessAmount):
+#     guessEntry = input("Pick a number between {} and {} " .format(numMin, numMax))
+# guess_type = "Rando"
+# guess_color = "pink"
 # firstGuess = guessEntry(452)
 #
 #
